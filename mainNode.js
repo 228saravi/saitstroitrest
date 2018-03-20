03c218b3d3e16b213c9e7c40539990e4bf75e06a
228saravi <228saravi@gmail.com>
2018-03-20 11:44:48 +0300

    1

---
+++ mainNode.js
@@ -0,0 +1,13 @@
+var express = require('express')
+var path = require('path');
+var app = express()
+var catalog = 'static'; 
+var catalog2 = 'node_module'; 
+app.use('/', express.static(path.join(__dirname ,catalog)));
+//app.use('/', express.static(path.join(__dirname ,catalog)));
+
+app.get('/', function(req, res){    
+    res.sendFile(path.join(__dirname, catalog, 'index.html'));
+});
+ 
+app.listen(3000)
